3de7038ed5f95ade550d6d28667fcb007947f056
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2026-06-18 22:25:31 +0300

    the logical operator

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const hasDriversLicense = true;
-const hasGoodVision = false;
+const hasDriversLicense = true; // есть удостоверение водителя
+const hasGoodVision = true;    //  хорошее зрение
 
-console.log(hasDriversLicense && hasDriversLicense);
-console.log(hasDriversLicense || hasDriversLicense);
-console.log(!hasDriversLicense );
+console.log(hasDriversLicense && hasGoodVision); // true
+console.log(hasDriversLicense || hasGoodVision); // true
+console.log(!hasDriversLicense);                 // false
 
-const shouldDrivers
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("можно сесть за руль");
+// } else {
+//   console.log("за руль сядет кто-то другой");
+// }
+
+const isTirad = true;  //устал
+console.log(hasDriversLicense && hasGoodVision && isTirad);
+
+ if (hasDriversLicense && hasGoodVision && !isTirad) {
+  console.log("можно сесть за руль");
+ } else {
+  console.log("за руль сядет кто-то другой");
+ }
