97c948f9e7580536bcf28706b6dc482d04b8ce86
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2026-06-19 23:09:47 +0300

    The winning dolphins

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const hasDriversLicense = true; // есть удостоверение водителя
+/*const hasDriversLicense = true; // есть удостоверение водителя
 const hasGoodVision = true;    //  хорошее зрение
 
 console.log(hasDriversLicense && hasGoodVision); // true
@@ -19,3 +19,16 @@ console.log(hasDriversLicense && hasGoodVision && isTirad);
  } else {
   console.log("за руль сядет кто-то другой");
  }
+*/
+
+const scoreDolphins = (96 + 108 + 89) / 3; // оценка дельфинов
+const scoreKoalas = (88 + 91 + 110) / 3; //оценка коалы
+console.log(scoreDolphins, scoreKoalas ,"😁")
+
+if(scoreDolphins>scoreKoalas){
+  console.log(`Dolphins win the trophey 🏆` )
+}else if(scoreDolphins<scoreKoalas){
+  console.log(` Koalas win the trophey 🏆`)
+}else if(scoreDolphins=scoreKoalas){
+console.log(`Both win the tropy!`)
+}
