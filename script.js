56299900ca4fbf3017680f4d22090d6b901690f7
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2026-06-21 21:54:44 +0300

    the switch operator

--- script.js
+++ script.js
@@ -33,16 +33,41 @@ console.log(hasDriversLicense && hasGoodVision && isTirad);
 // console.log(`Both win the tropy!`)
 // }
 
-const scoreDolphins = (97 + 112 + 80) / 3; // оценка дельфинов
-const scoreKoalas = (109 + 95 + 50) / 3; //оценка коалы
-console.log(scoreDolphins, scoreKoalas, "😁");
+// const scoreDolphins = (97 + 112 + 80) / 3; // оценка дельфинов
+// const scoreKoalas = (109 + 95 + 50) / 3; //оценка коалы
+// console.log(scoreDolphins, scoreKoalas, "😁");
 
-if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
-  console.log(`Dolphins win the trophey 🏆`);
-} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
-  console.log(` Koalas win the trophey 🏆`);
-} else if ((scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >=100)) {
-  console.log(`Both win the tropy!`);
-}else {
-  console.log(`No one wins the  trophy`)
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//   console.log(`Dolphins win the trophey 🏆`);
+// } else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+//   console.log(` Koalas win the trophey 🏆`);
+// } else if ((scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >=100)) {
+//   console.log(`Both win the tropy!`);
+// }else {
+//   console.log(`No one wins the  trophy`)
+// }
+
+const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log(`plan structure`);
+    console.log(`go to coding meetup`);
+    break;
+  case "tuesday":
+    console.log(`prepare theory videos`);
+    break;
+  case "wenesday":
+  case "thursay":
+    console.log(`write code examples`);
+    break;
+  case "friday":
+    console.log("record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day!");
 }
