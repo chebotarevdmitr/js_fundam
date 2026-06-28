340b102a03acb156df9ce861abd030c3a369a31d
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2026-06-28 23:03:12 +0300

    javascript function implementation

--- script2.js
+++ script2.js
@@ -1,7 +1,23 @@
 `use strict`; //enabling strict mode  -включение строгого режима
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if(hasDriversLicense) console.log(`If you have a driver's license, you can drive.`); //если у тебя есть водительские права можешь вести машину
+// if (passTest) hasDriversLicense = true;
+// if(hasDriversLicense) console.log(`If you have a driver's license, you can drive.`); //если у тебя есть водительские права можешь вести машину
+
+function logger() {
+  console.log(`My name is Dmitry.`);
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+ // console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const applesJuice = fruitProcessor(5, 10);
+console.log (applesJuice);
+console.log(fruitProcessor(3,4))
